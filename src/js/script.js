ce49aaf1652ee40b8352af444cbffdcbcca3582c
Vyacheslav-Belov-OO <vyacheslav.belov@onlyoffice.com>
2023-05-02 15:36:10 +0300

    add sticky menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,15 @@
-Fancybox.bind("[data-fancybox]", {
-    // Your custom options
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () =>{
+  const menu = document.querySelector('.menu_top_header');
+ 
+  window.addEventListener('scroll', e => {
+    if(window.scrollY >= 100) {
+      menu.classList.add('sticky');
+    }
+    else {
+      menu.classList.remove('sticky');
+    }
   });
+
+});
